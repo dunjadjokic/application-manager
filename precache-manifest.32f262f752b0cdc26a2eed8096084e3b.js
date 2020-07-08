3c61b5be555f45a2f1ebaed096acf572c385d0d9
dunjadjokic <43410377+dunjadjokic@users.noreply.github.com>
2020-07-08 12:48:42 +0000

    Deploying to gh-pages from  @ f13717b5c9cf893be44b536736473c7419687547 🚀

---
+++ precache-manifest.32f262f752b0cdc26a2eed8096084e3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44351da46c41c805d2e1c4f137667e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4951dcf451a02e819af",
+    "url": "/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "c094d091cb9f06330cfd",
+    "url": "/static/css/main.5a6dbe76.chunk.css"
+  },
+  {
+    "revision": "c4951dcf451a02e819af",
+    "url": "/static/js/2.07044dfd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.07044dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c094d091cb9f06330cfd",
+    "url": "/static/js/main.aa057dca.chunk.js"
+  },
+  {
+    "revision": "258f3d354e7516659308",
+    "url": "/static/js/runtime-main.9856ee78.js"
+  }
+]);
